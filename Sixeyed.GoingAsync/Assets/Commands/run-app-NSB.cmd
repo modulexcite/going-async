494e7fdbd4d4f14e3c325264364d147cdbb046fe
Mauro Servienti <mauro@topics.it>
2015-05-21 10:08:50 +0200

    small glitches after merge

--- Sixeyed.GoingAsync/Assets/Commands/run-app-NSB.cmd
+++ Sixeyed.GoingAsync/Assets/Commands/run-app-NSB.cmd
@@ -1,11 +1,9 @@
-
-
 start "Validator" /D C:\temp\going-async\Sixeyed.GoingAsync.AppV2.Consumer C:\temp\going-async\Sixeyed.GoingAsync.AppV2.Consumer\Sixeyed.GoingAsync.AppV2.Consumer.exe /e Sixeyed.GoingAsync.AppV2.Consumer.trade-validate
 start "Party1Enricher" /D C:\temp\going-async\Sixeyed.GoingAsync.AppV2.Consumer C:\temp\going-async\Sixeyed.GoingAsync.AppV2.Consumer\Sixeyed.GoingAsync.AppV2.Consumer.exe /e Sixeyed.GoingAsync.AppV2.Consumer.trade-enrich-party1
 start "Party2Enricher" /D C:\temp\going-async\Sixeyed.GoingAsync.AppV2.Consumer C:\temp\going-async\Sixeyed.GoingAsync.AppV2.Consumer\Sixeyed.GoingAsync.AppV2.Consumer.exe /e Sixeyed.GoingAsync.AppV2.Consumer.trade-enrich-party2
 
 
-rem sqlcmd -S .\SqlExpress -d GoingAsync -Q "truncate table incomingtrades"
-rem del /Q C:\temp\going-async\in\app-nsb\*.*
-rem cd C:\temp\going-async\Sixeyed.GoingAsync.Tools.TradeGenerator\Sixeyed.GoingAsync.Tools.TradeGenerator.exe /a v2 /c 100
-rem cd C:\temp\going-async\Sixeyed.GoingAsync.AppV2.Producer\Sixeyed.GoingAsync.AppV2.Producer.exe
+sqlcmd -S .\SqlExpress -d GoingAsync -Q "truncate table incomingtrades"
+del /Q C:\temp\going-async\in\app-nsb\*.*
+start C:\temp\going-async\Sixeyed.GoingAsync.Tools.TradeGenerator\Sixeyed.GoingAsync.Tools.TradeGenerator.exe /a v2 /c 10000
+start C:\temp\going-async\Sixeyed.GoingAsync.AppV2.Producer\Sixeyed.GoingAsync.AppV2.Producer.exe
